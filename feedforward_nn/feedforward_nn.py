718ae2f8fc402a04747f838f126dc4585c9111c1
bozkurt <emrebzkurt1@gmail.com>
2024-11-16 14:37:16 +0300

    WIP

--- feedforward_nn/feedforward_nn.py
+++ feedforward_nn/feedforward_nn.py
@@ -10,23 +10,27 @@ class FeedForwardNetwork:
         self.init_weights()
 
     def init_weights(self):
-        self.weights = np.random.normal(size=(self.input_dim, self.num_of_nodes, self.num_hidden_layers))
-        self.bias = np.random.normal(size=(self.num_of_nodes, self.num_hidden_layers))
+        self.weights = {}
+        self.weights[0] = np.random.normal(size=(self.input_dim, self.num_of_nodes, 1))
+        self.weights[1] = np.random.normal(size=(self.num_of_nodes, self.num_of_nodes, self.num_hidden_layers))
+        self.weights[2] = np.random.normal(size=(self.num_of_nodes, self.output_dim, 1))
 
+        self.bias = {}
+        self.bias[0] = np.random.normal(size=(self.num_of_nodes, 1))
+        self.bias[1] = np.random.normal(size=(self.num_of_nodes, self.num_hidden_layers))
+        self.bias[2] = np.random.normal(size=(self.output_dim, 1))
+        
     def forward(self, input):
         forward_input = input
         for i in range(self.num_hidden_layers):
-            print(f'Processing layer {i}')
-            layer_weights = self.weights[:, :, i]
-            layer_bias = self.bias[:, i]
-            forward_input = np.dot(layer_weights, forward_input) + layer_bias
+            w_key = 0 if i == 0 else (2 if i == self.num_hidden_layers - 1 else 1)
 
 
 
 
 if __name__ == '__main__':
     input = np.random.normal(size=(5,1))
-    ffnn = FeedForwardNetwork(input.shape[0], 1, 2, 5)
+    ffnn = FeedForwardNetwork(input.shape[0], 1, 7, 10)
 
     ffnn.forward(input)
 
